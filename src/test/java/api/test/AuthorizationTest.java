b0f0574fa57d5a6fba5d8a03db0271a2fe6752c5
ivalko-corcentric <87310986+ivalko-corcentric@users.noreply.github.com>
2023-09-12 15:45:49 -0500

    make auth test parametrized, added auth failed test

--- src/test/java/api/test/AuthorizationTest.java
+++ src/test/java/api/test/AuthorizationTest.java
@@ -1,8 +1,10 @@
 package api.test;
 
 import api.test.entity.CreateTokenResponse;
+import api.test.entity.ErrorResponse;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -11,18 +13,18 @@ import static org.testng.Assert.assertNotNull;
 
 public class AuthorizationTest {
 
-    @Test
-     public void authToken(){
+
+    @Test(dataProvider = "inputCorrectValues")
+    public void authSuccessTest(String username, String password) {
         //Arrange
 
         String addURI = "https://restful-booker.herokuapp.com/auth";
 
-        String payload = """
-                {
-                    "username" : "admin",
-                    "password" : "password123"
-                }
-                """;
+        String payload = "{\n" +
+                "    \"username\" : \"" + username + "\",\n" +
+                "    \"password\" : \"" + password + "\"\n" +
+                "}";
+        
         HttpHeaders headers = new HttpHeaders();
 
         headers.add("Accept", "application/json");
@@ -44,4 +46,52 @@ public class AuthorizationTest {
         assertNotNull(responseBody);
         assertNotNull(responseBody.getToken());
     }
+
+    @Test(dataProvider = "inputValuesInvalid")
+    public void authFailTest(String username, String password) {
+        //Arrange
+        String addURI = "https://restful-booker.herokuapp.com/auth";
+
+        String payload = "{\n" +
+                "    \"username\" : \"" + username + "\",\n" +
+                "    \"password\" : \"" + password + "\"\n" +
+                "}";
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Accept", "application/json");
+        headers.add("Content-Type", "application/json");
+
+
+        HttpEntity<String> entity = new HttpEntity<>(payload, headers);
+
+        //Act
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<ErrorResponse> response = restTemplate.exchange(addURI, HttpMethod.POST, entity, ErrorResponse.class);
+
+        //Assert
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        ErrorResponse responseBody = response.getBody();
+        assertNotNull(responseBody);
+        assertEquals(responseBody.getReason(), "Bad credentials");
+
+    }
+
+
+    @DataProvider(name = "inputValuesInvalid")
+    public Object[][] valuesUsernameAndPassword() {
+        return new Object[][]{
+                {"qwerty", "2"},
+                {"test", "test1"},
+                {"", "0"}
+        };
+    }
+
+    @DataProvider(name = "inputCorrectValues")
+    public Object[][] valuesCorrectUsernameAndPassword(){
+        return new Object[][] {
+                {"admin", "password123"}
+        };
+    }
+
 }
---
+++ src/test/java/api/test/entity/ErrorResponse.java
@@ -0,0 +1,9 @@
+package api.test.entity;
+
+import lombok.Data;
+
+@Data
+public class ErrorResponse {
+
+    private String reason;
+}
